01d0e3c3a46781c308558bdcf33db1061e335483
ant4g0nist <ichaithu@gmail.com>
2020-12-30 14:55:07 +0100

    close capstone handle

--- arm64_disasm.c
+++ arm64_disasm.c
@@ -20,19 +20,20 @@ char *arm_disassemble64( char * sect, unsigned long left, cpu_type_t cputype)
 	cs_insn *insn;
 	exc_handler_arm64_disas[0]=0;
 
-    if (cs_open(CS_ARCH_ARM64, CS_MODE_ARM, &handle) != CS_ERR_OK)
-    {
-        return exc_handler_arm64_disas;    
-    }
+	if (cs_open(CS_ARCH_ARM64, CS_MODE_ARM, &handle) != CS_ERR_OK)
+	{
+		return exc_handler_arm64_disas;    
+	}
 
-    cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON); // turn ON detail feature with CS_OPT_ON
-    size_t count = cs_disasm(handle, sect, 1024, 0, 0, &insn);
-    for(int i=0; i< count; i++)
-    {
-        cs_detail *detail = insn[i].detail;
-        printf("%s\t%s\n", insn[i].mnemonic, insn[i].op_str);
-        break;
-    }
-
-    return exc_handler_arm64_disas;
+	cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON); // turn ON detail feature with CS_OPT_ON
+	size_t count = cs_disasm(handle, sect, 1024, 0, 0, &insn);
+	for(int i=0; i< count; i++)
+	{
+		cs_detail *detail = insn[i].detail;
+		printf("%s\t%s\n", insn[i].mnemonic, insn[i].op_str);
+		break;
+	}
+	
+	cs_close(&handle);
+	return exc_handler_arm64_disas;
 }
